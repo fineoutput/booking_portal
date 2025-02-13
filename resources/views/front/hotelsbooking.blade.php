7149af37b6f746b716275ae4d0176166346a513f
Ayush <ayushrs7023@gmail.com>
2025-02-13 15:32:28 +0530

    hotels

--- resources/views/front/hotelsbooking.blade.php
+++ resources/views/front/hotelsbooking.blade.php
@@ -6,7 +6,7 @@
   <div class="header-container_hotels">
     <div class="search-header_hotels">
       <!-- Destination Dropdown -->
-      <div class="filter-item_hotels" onclick="toggleDropdown('destination')">
+      <div class="filter-item_hotels sachi" onclick="toggleDropdown('destination')">
         <div class="filter-label_hotels">Destination</div>
         <div class="filter-value_hotels" id="destination-value">Where are you going?</div>
         <div class="dropdown_hotels destination-dropdown_hotels" id="destination-dropdown">
@@ -19,19 +19,19 @@
       </div>
 
       <!-- Check-in Date -->
-      <div class="filter-item_hotels">
+      <div class="filter-item_hotels sachi">
         <div class="filter-label_hotels">Check in</div>
         <input type="date" class="filter-value_hotels">
       </div>
 
       <!-- Check-out Date -->
-      <div class="filter-item_hotels">
+      <div class="filter-item_hotels sachi">
         <div class="filter-label_hotels">Check out</div>
         <input type="date" class="filter-value_hotels">
       </div>
 
       <!-- Guests Dropdown -->
-      <div class="filter-item_hotels" onclick="toggleDropdown('guests')">
+      <div class="filter-item_hotels sachi" onclick="toggleDropdown('guests')">
         <div class="filter-label_hotels">Guests</div>
         <div class="filter-value_hotels" id="guests-value">1 guest</div>
         <div class="dropdown_hotels guests-dropdown_hotels" id="guests-dropdown">
@@ -61,9 +61,195 @@
           </div>
         </div>
       </div>
+
+      <div class="search_sba">
+        <div class="sba_center_Sarch">
+        <a href="#">  
+        <img src="http://127.0.0.1:8000/frontend/images/search.png" alt="" style="
+    width: 80%;
+">
+        </a>  
+      </div>
+      </div>
     </div>
   </div>
 
+
+  <section class="_hotels_filters">
+    <div class="container-fluid">
+      <div class="row">
+      <div class="col-lg-2">
+  <div class="alocate_hotel">
+    <!-- Splide Slider -->
+    <div class="splide alocate_slider">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/first.avif" alt="Image 1">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/secound.avif" alt="Image 2">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/third.avif" alt="Image 3">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="alocate_title_data">
+      <div class="ttiel_head">
+        <h4 class="size">Mashroop, India</h4>
+        <h4 class="key">Mountain Views</h4>
+        <h4 class="path key">13-18 Feb</h4>
+        <h4 class="seeve size">₹18,806 night</h4>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-2">
+  <div class="alocate_hotel">
+    <!-- Splide Slider -->
+    <div class="splide alocate_slider">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/third.avif" alt="Image 1">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/secound.avif" alt="Image 2">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/fourth.avif" alt="Image 3">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="alocate_title_data">
+      <div class="ttiel_head">
+        <h4 class="size">Mashroop, India</h4>
+        <h4 class="key">Mountain Views</h4>
+        <h4 class="path key">13-18 Feb</h4>
+        <h4 class="seeve size">₹18,806 night</h4>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="col-lg-2">
+  <div class="alocate_hotel">
+    <!-- Splide Slider -->
+    <div class="splide alocate_slider">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/fourth.avif" alt="Image 1">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/secound.avif" alt="Image 2">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/fifth.avif" alt="Image 3">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="alocate_title_data">
+      <div class="ttiel_head">
+        <h4 class="size">Mashroop, India</h4>
+        <h4 class="key">Mountain Views</h4>
+        <h4 class="path key">13-18 Feb</h4>
+        <h4 class="seeve size">₹18,806 night</h4>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="col-lg-2">
+  <div class="alocate_hotel">
+    <!-- Splide Slider -->
+    <div class="splide alocate_slider">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/sixth.avif" alt="Image 1">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/secound.avif" alt="Image 2">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/first.avif" alt="Image 3">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="alocate_title_data">
+      <div class="ttiel_head">
+        <h4 class="size">Mashroop, India</h4>
+        <h4 class="key">Mountain Views</h4>
+        <h4 class="path key">13-18 Feb</h4>
+        <h4 class="seeve size">₹18,806 night</h4>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="col-lg-2">
+  <div class="alocate_hotel">
+    <!-- Splide Slider -->
+    <div class="splide alocate_slider">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/third.avif" alt="Image 1">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/secound.avif" alt="Image 2">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/fourth.avif" alt="Image 3">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="alocate_title_data">
+      <div class="ttiel_head">
+        <h4 class="size">Mashroop, India</h4>
+        <h4 class="key">Mountain Views</h4>
+        <h4 class="path key">13-18 Feb</h4>
+        <h4 class="seeve size">₹18,806 night</h4>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="col-lg-2">
+  <div class="alocate_hotel">
+    <!-- Splide Slider -->
+    <div class="splide alocate_slider">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/third.avif" alt="Image 1">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/secound.avif" alt="Image 2">
+          </li>
+          <li class="splide__slide new_lave">
+            <img src="frontend/images/fourth.avif" alt="Image 3">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="alocate_title_data">
+      <div class="ttiel_head">
+        <h4 class="size">Mashroop, India</h4>
+        <h4 class="key">Mountain Views</h4>
+        <h4 class="path key">13-18 Feb</h4>
+        <h4 class="seeve size">₹18,806 night</h4>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </section>
 <script>
      // Toggle dropdowns
      function toggleDropdown(type) {
@@ -110,6 +296,24 @@
       document.getElementById('guests-value').textContent =
         `${totalGuests} guest${totalGuests !== 1 ? 's' : ''}`;
     }
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var sliders = document.querySelectorAll('.alocate_slider');
+    
+    sliders.forEach(function (slider) {
+      new Splide(slider, {
+        type: 'fade', // or 'loop' for infinite scroll
+        perPage: 1,
+        autoplay: true,
+        interval: 3000, // 3 seconds delay
+        arrows: false, 
+        pagination: true,
+      }).mount();
+    });
+  });
+
 </script>
 
 @endsection
