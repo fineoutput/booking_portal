947353698e4d397b8b12f06243f4e0f3388973fb
Ayush <ayushrs7023@gmail.com>
2025-02-20 18:48:28 +0530

    edit

--- resources/views/admin/route/edit.blade.php
+++ resources/views/admin/route/edit.blade.php
@@ -2,26 +2,24 @@
 @section('main')
 <!-- Start content -->
 <style>
-  
     form {
-      margin-top: 20px;
+        margin-top: 20px;
     }
     
     select {
-      width: 400px;
+        width: 400px;
     }
-    </style>
+</style>
 
 <div class="content">
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12">
                 <div class="page-title-box">
-                    <h4 class="page-title">Add Route</h4>
+                    <h4 class="page-title">Edit Route</h4> <!-- Updated title -->
                     <ol class="breadcrumb" style="display:none">
-                        <!-- <li class="breadcrumb-item"><a href="javascript:void(0);">CMS</a></li> -->
                         <li class="breadcrumb-item"><a href="javascript:void(0);">Route</a></li>
-                        <li class="breadcrumb-item active">Add Route</li>
+                        <li class="breadcrumb-item active">Edit Route</li>
                     </ol>
                 </div>
             </div>
@@ -37,28 +35,30 @@
                             <div class="alert alert-success" role="alert">
                                 {{ session('success') }}
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
+                                    <span aria-hidden="true">×</span>
+                                </button>
                             </div>
                             @endif
                             @if (session('error'))
                             <div class="alert alert-danger" role="alert">
                                 {{ session('error') }}
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
+                                    <span aria-hidden="true">×</span>
+                                </button>
                             </div>
                             @endif
                             <!-- End show success and error messages -->
-                            <h4 class="mt-0 header-title">Add Route Form</h4>
+                            <h4 class="mt-0 header-title">Edit Route Form</h4> <!-- Updated header -->
                             <hr style="margin-bottom: 50px;background-color: darkgrey;">
                             <form action="{{ route('route.update', $route->id) }}" method="post" enctype="multipart/form-data">
                                 @csrf
-                                @method('PUT')  <!-- This line is for making the form method PUT for an update operation -->
-                            
+                                @method('PUT') <!-- For update operation -->
+
                                 <div class="form-group row">
                                     <div class="col-sm-4">
                                         <div class="form-floating">
                                             <input type="text" class="form-control" value="{{ old('from_destination', $route->from_destination) }}" id="from_destination" name="from_destination" placeholder="Enter name" required>
-                                            <label for="from_destination">Enter Starting Destination &nbsp;<span style="color:red;">*</span></label>
+                                            <label for="from_destination">Enter Starting Destination <span style="color:red;">*</span></label>
                                         </div>
                                         @error('from_destination')
                                         <div style="color:red">{{ $message }}</div>
@@ -67,17 +67,32 @@
                                     <div class="col-sm-4">
                                         <div class="form-floating">
                                             <input class="form-control" type="text" value="{{ old('to_destination', $route->to_destination) }}" id="to_destination" name="to_destination" placeholder="Enter Ending Destination" required>
-                                            <label for="to_destination">Enter Ending Destination &nbsp;<span style="color:red;">*</span></label>
+                                            <label for="to_destination">Enter Ending Destination <span style="color:red;">*</span></label>
                                         </div>
                                         @error('to_destination')
                                         <div style="color:red">{{ $message }}</div>
                                         @enderror
                                     </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label for="city_image">City Image</label>
+                                            <input type="file" class="form-control-file" id="city_image" name="city_image" accept="image/*">
+                                            @if ($route->city_image)
+                                                <div style="margin-top: 10px;">
+                                                    <img src="{{ asset('storage/' . $route->city_image) }}" alt="Current City Image" style="max-width: 100px; max-height: 100px;">
+                                                    <p><small>Current Image: {{ $route->city_image }}</small></p>
+                                                </div>
+                                            @endif
+                                        </div>
+                                        @error('city_image')
+                                        <div style="color:red">{{ $message }}</div>
+                                        @enderror
+                                    </div>
                                 </div>
-                            
+
                                 <div class="form-group">
                                     <div class="w-100 text-center">
-                                        <button type="submit" style="margin-top: 10px;" class="btn btn-danger"><i class="fa fa-user"></i> Submit</button>
+                                        <button type="submit" style="margin-top: 10px;" class="btn btn-danger"><i class="fa fa-user"></i> Update</button> <!-- Changed to "Update" -->
                                     </div>
                                 </div>
                             </form>
@@ -89,6 +104,5 @@
         <!-- end page content-->
     </div> <!-- container-fluid -->
 </div> <!-- content -->
-
 
 @endsection
