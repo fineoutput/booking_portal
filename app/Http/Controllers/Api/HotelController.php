4e742013a206f679efdfa570308ed715ac004df8
Nitesh Shah <niteshshah000@gmail.com>
2025-04-18 12:43:17 +0530

    //

--- app/Http/Controllers/Api/HotelController.php
+++ app/Http/Controllers/Api/HotelController.php
@@ -213,7 +213,6 @@ class HotelController extends Controller
 
 
 
-
 public function package(Request $request)
 {
     $token = $request->bearerToken();
@@ -236,14 +235,27 @@ public function package(Request $request)
         $stateId = $request->input('state_id');
         $cityId = $request->input('city_id');
 
+        // Get price filter from the request, with default values
+        $min_price = $request->input('min_price', 0);
+        $max_price = $request->input('max_price', 10000000);
+
         $query = Package::query();
 
+        // Apply state and city filters if provided
         if ($stateId) {
             $query->where('state_id', $stateId);
         }
 
         if ($cityId) {
             $query->where('city_id', $cityId);
+        }
+
+        // Apply price filters if they are provided
+        if ($min_price > 0 || $max_price < 10000000) {
+            $query->whereHas('packagePrices', function ($query) use ($min_price, $max_price) {
+                $query->whereRaw('CAST(display_cost AS UNSIGNED) >= ?', [$min_price])
+                      ->whereRaw('CAST(display_cost AS UNSIGNED) <= ?', [$max_price]);
+            });
         }
 
         $packages = $query->get();
@@ -266,7 +278,7 @@ public function package(Request $request)
                 ->get();
 
             $prices = null;
-            if($packagePrice) {
+            if ($packagePrice) {
                 $prices = $packagePrice->map(function($price) {
                     return [
                         'id' => $price->id,
@@ -332,6 +344,126 @@ public function package(Request $request)
         'status' => 201,
     ], 401);
 }
+
+
+// public function package(Request $request)
+// {
+//     $token = $request->bearerToken();
+
+//     if (!$token) {
+//         return response()->json([
+//             'message' => 'Unauthenticated.',
+//             'data' => [],
+//             'status' => 201,
+//         ], 401);
+//     }
+
+//     $decodedToken = base64_decode($token);
+//     list($email, $password) = explode(',', $decodedToken);
+
+//     $user = Agent::where('email', $email)->first();
+
+//     if ($user && $password == $user->password) {
+
+//         $stateId = $request->input('state_id');
+//         $cityId = $request->input('city_id');
+
+//         $query = Package::query();
+
+//         if ($stateId) {
+//             $query->where('state_id', $stateId);
+//         }
+
+//         if ($cityId) {
+//             $query->where('city_id', $cityId);
+//         }
+
+//         $packages = $query->get();
+
+//         $states = State::all()->pluck('state_name', 'id');
+//         $cities = City::all()->pluck('city_name', 'id');
+
+//         $hotelData = $packages->map(function($package) use ($states, $cities) {
+
+//             $imageUrls = array_values(json_decode($package->image, true));
+
+//             $stateNames = $this->getNamesByIds($package->state_id, $states);
+//             $cityNames = $this->getNamesByIds($package->city_id, $cities);
+
+//             $today = Carbon::today()->format('Y-m-d');
+
+//             $packagePrice = PackagePrice::where('package_id', $package->id)
+//                 ->where('start_date', '<=',$today)  
+//                 ->where('end_date', '>=',$today) 
+//                 ->get();
+
+//             $prices = null;
+//             if($packagePrice) {
+//                 $prices = $packagePrice->map(function($price) {
+//                     return [
+//                         'id' => $price->id,
+//                         'start_date' => Carbon::parse($price->start_date)->format('F Y'),
+//                         'end_date' => Carbon::parse($price->end_date)->format('F Y'),
+//                         'display_price' => $price->display_cost,
+//                         'total_cost' => 
+//                         $price->standard_cost ?? 0 +
+//                         $price->deluxe_cost ?? 0 +
+//                         $price->premium_cost ?? 0 +
+//                         $price->super_deluxe_cost ?? 0 +
+//                         $price->luxury_cost ?? 0 +
+//                         $price->nights_cost ?? 0 +
+//                         $price->adults_cost ?? 0 +
+//                         $price->child_with_bed_cost ?? 0 +
+//                         $price->child_no_bed_infant_cost ?? 0 +
+//                         $price->child_no_bed_child_cost ?? 0 +
+//                         $price->meal_plan_only_room_cost ?? 0 +
+//                         $price->meal_plan_breakfast_cost ?? 0 +
+//                         $price->meal_plan_breakfast_lunch_dinner_cost ?? 0 +
+//                         $price->meal_plan_all_meals_cost ?? 0 +
+//                         $price->hatchback_cost ?? 0 +
+//                         $price->sedan_cost ?? 0 +
+//                         $price->economy_suv_cost ?? 0 +
+//                         $price->luxury_suv_cost ?? 0 +
+//                         $price->traveller_mini_cost ?? 0 +
+//                         $price->traveller_big_cost ?? 0 +
+//                         $price->premium_traveller_cost ?? 0 +
+//                         $price->ac_coach_cost ?? 0 +
+//                         $price->extra_bed_cost ?? 0
+//                     ];
+//                 });
+//             }
+
+//             return [
+//                 'id' => $package->id,
+//                 'package_name' => $package->package_name,
+//                 'state_names' => $stateNames, 
+//                 'city_names' => $cityNames,
+//                 'image' => array_map(function($image) {
+//                     return url('') . '/' . $image;
+//                 }, $imageUrls),
+//                 'video' => array_map(function($video) {
+//                     return url('') . '/' . $video;
+//                 }, is_array($videos = json_decode($package->video, true)) ? $videos : []),
+//                 'pdf' => url('') . '/' . $package->pdf,
+//                 'text_description' => strip_tags($package->text_description),
+//                 'text_description_2' => strip_tags($package->text_description_2),
+//                 'prices' => $prices,
+//             ];
+//         });
+
+//         return response()->json([
+//             'message' => 'Package fetched successfully.',
+//             'data' => $hotelData,
+//             'status' => 200
+//         ], 200);
+//     }
+
+//     return response()->json([
+//         'message' => 'Unauthenticated',
+//         'data' => [],
+//         'status' => 201,
+//     ], 401);
+// }
 
     // public function package(Request $request)
     // {
