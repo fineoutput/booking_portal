5d241a527913b186db57035303eae7a734340eff
Ayush <ayushrs7023@gmail.com>
2025-05-17 13:48:42 +0530

    //

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -607,7 +607,7 @@
                 <div class="destination">
                   <p style="margin: 0;">{{\Illuminate\Support\Str::limit($value->package_name ?? '', 15) }}</p>
                   
-                  @foreach($value->hotels as $hotel)
+                  @foreach($value->hotels->take(5) as $hotel)
                    
                     <span>{{ \Illuminate\Support\Str::limit($hotel->name ?? '', 10) }}</span>,
                   @endforeach
