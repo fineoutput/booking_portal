b90f243cc3d3675a72cd18b8eeef38e4e2826862
Ayush <ayushrs7023@gmail.com>
2025-06-19 18:45:16 +0530

    </b>

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -641,7 +641,7 @@
                   </div>
                   <div class="BEth">
                     <div class="BetSide">
-                      <h5>{{ \Illuminate\Support\Str::limit($value->package_name ?? '', 10) }}</h5>
+                      <h6><b>{{ \Illuminate\Support\Str::limit($value->package_name ?? '', 10) }}</b></h6>
                       <p>{{ \Carbon\Carbon::now()->format('D, d M') }}</p>
                       @if($value->prices)
                         <p>₹{{ number_format($value->prices->display_cost, 2) }} onwards</p>
