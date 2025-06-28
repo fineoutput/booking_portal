6b5461a951b54819c27dbfce6d9252b3682a82f5
Ayush <ayushrs7023@gmail.com>
2025-06-28 14:42:33 +0530

    Hotel Bookings

--- resources/views/front/hotelsbooking.blade.php
+++ resources/views/front/hotelsbooking.blade.php
@@ -157,10 +157,9 @@
   </div>
 
   @if($slider)
-  <div id="responsive-slider" class="splide" style="background: #ffd600">
+  {{-- <div id="responsive-slider" class="splide" style="background: #ffd600">
     <div class="layie">
-      {{-- <h1>Plan Your Travel Now!</h1>
-                        <p>650+ Travel Agents serving 65+ Destinations worldwide</p> --}}
+     
                       </div>  
   <div class="splide__track">
       
@@ -177,18 +176,10 @@
           @endforeach
          
             
-            {{-- <li class="splide__slide"> 
-                <picture>
-                    <source media="(min-width: 1200px)" srcset="{{ asset('frontend/images/banner/banne.png') }}">
-                    <source media="(min-width: 768px)" srcset="{{ asset('frontend/images/banner/banne.png') }}">
-                    <source media="(max-width: 767px)" srcset="{{ asset('frontend/images/banner/mobile_.png') }}">
-                    <img style="border-radius: 0;" src="{{ asset('frontend/images/banner/fallback_.png') }}" alt="Responsive Banner 2">
-                </picture>
-            </li> --}}
-            <!-- Add more slides as needed -->
+
         </ul>
     </div>
-</div>
+</div> --}}
 @endif
 
   <section class="_hotels_filters">
