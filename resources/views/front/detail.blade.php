cb6b8d074176a0d9565a56d83380b63d6f601e38
Nitesh Shah <niteshshah000@gmail.com>
2025-07-11 16:36:31 +0530

    //

--- resources/views/front/detail.blade.php
+++ resources/views/front/detail.blade.php
@@ -694,7 +694,7 @@
 
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
 <script>
     const packagePrices = @json($packages->packagePrices ?? []);
 
