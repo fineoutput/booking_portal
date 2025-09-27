fcd0ef643ad6dbd6b3f8ee1eb3e165730df41e2c
Nitesh Shah <niteshshah000@gmail.com>
2025-09-27 12:31:15 +0530

    //

--- app/Http/Controllers/Api/HotelController.php
+++ app/Http/Controllers/Api/HotelController.php
@@ -2108,9 +2108,9 @@ public function hotelcitys(Request $request)
         $meal_cost_total = $meal_cost * $request->room_count * $nightCount;
         $extra_bed_total = $extra_bed_cost * $extra_bed * $nightCount;
         $child_nobed_total = $no_bed_child_cost * $nobed * $nightCount;
-        $base_room_cost = $request->room_count * $price->night_cost * $nightCount;
+        // $base_room_cost = $request->room_count * $price->night_cost * $nightCount;
 
-        $final_cost = $meal_cost_total + $extra_bed_total + $child_nobed_total + $base_room_cost;
+        $final_cost = $meal_cost_total + $extra_bed_total + $child_nobed_total;
 
         // -------------------------------
         // Save Booking
