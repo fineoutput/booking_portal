2abcedf8fc156170f27ce152d2c1451a50de435f
Nitesh Shah <niteshshah000@gmail.com>
2025-11-24 12:16:56 +0530

    //

--- app/Http/Controllers/Api/HotelController.php
+++ app/Http/Controllers/Api/HotelController.php
@@ -3236,13 +3236,13 @@ public function confirm(Request $request)
             return response()->json(['message' => 'Package booking not found','status' => 201], 404);
         }
     
-        $final_price = ($packagetempbooking->total_cost ?? 0) + ($request->agent_margin ?? 0);
+        $final_price = ($packagetempbooking->cost ?? 0) + ($request->agent_margin ?? 0);
     
         $packagebooking = new TripGuideBook2();
         $packagebooking->guide_order_id = $request->guide_id;
         $packagebooking->user_id = $packagetempbooking->user_id;
         $packagebooking->guide_id = $packagetempbooking->tour_guide_id;
-        $packagebooking->fetched_price = $packagetempbooking->total_cost;
+        $packagebooking->fetched_price = $packagetempbooking->cost;
         $packagebooking->agent_margin = $request->agent_margin;
         $packagebooking->final_price = $final_price;
         $packagebooking->salesman_name = $request->salesman_name;
