ac3be201c331dad79058bd3684278f701973a691
Ayush <ayushrs7023@gmail.com>
2026-03-12 18:01:12 +0530

    //

--- resources/views/front/common/header.blade.php
+++ resources/views/front/common/header.blade.php
@@ -331,6 +331,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark nave_color ">
   <div class="container justify-content-center ">
     <a class="navbar-brand" href="{{ route('index') }}">Tours Dekho</a>
+    <a class="navbar-brand" style="visibility: hidden" href="{{ route('index') }}">Tours Dekho</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
