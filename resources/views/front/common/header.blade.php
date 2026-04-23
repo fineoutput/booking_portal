bbd95115b9f272542db196fa1abbc3cfa642b3ca
Ayush <ayushrs7023@gmail.com>
2026-04-23 15:45:50 +0530

    //

--- resources/views/front/common/header.blade.php
+++ resources/views/front/common/header.blade.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="google-site-verification" content="nVGH2Bh-2yb0KADFaujzEMO8cYFTxH9mlT6aCbgHqjA" />
   <title>Tours Dekho</title>
   <!-- Bootstrap CSS -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
